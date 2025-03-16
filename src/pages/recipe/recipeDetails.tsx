c00a53493e02f9d01f2f3c9b59d88d29f1dc6fb3
kanishka464 <kanishkadoulia@gmail.com>
2025-03-16 12:38:46 +0530

    Add: User profile details page

--- src/pages/recipe/recipeDetails.tsx
+++ src/pages/recipe/recipeDetails.tsx
@@ -1,9 +1,10 @@
-import { AccessTime, FavoriteBorder, Group, Groups, Share } from "@mui/icons-material";
+import { AccessTime, FavoriteBorder, Group, Share } from "@mui/icons-material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { calculateDifference } from "@/utils/helper";
 import { Recipe } from "@/utils/interface";
+import RecipeListing from "@/components/common/RecipeListing";
 
 const RecipeDetails = () => {
     const { id } = useParams();
@@ -176,52 +177,9 @@ const RecipeDetails = () => {
                     <div className="text-3xl font-bold">
                         You Might Also Like
                     </div>
+                    
+                    <RecipeListing recipes={recipeDetails?.similarRecipes}/>
 
-                    <div className="grid grid-cols-3 gap-5">
-                        {
-                            recipeDetails?.similarRecipes?.map((recipe:any) => (
-                                <div key={recipe?._id} className="flex flex-col w-full rounded-xl">
-                                    {/* Recipe Image */}
-                                    <Link to={`${window.location.origin}/recipes/${recipe?._id}`} className="w-full h-48 rounded-t-xl">
-                                        <img src={recipe?.recipeImage} className="h-52 max-h-52 w-full object-cover rounded-xl"/>
-                                    </Link>
-
-                                    {/* Recipe Detail */}
-                                    <div className="flex flex-col gap-3 p-5 bg-[#f9f9f9] rounded-b-xl">
-                                        <div className="flex justify-between items-center">
-                                        <Link to={recipe?._id} className="font-semibold text-xl">
-                                                {recipe?.recipeName}
-                                            </Link>
-                                        </div>
-
-                                        {/* Recipe Tags */}
-                                        <div className="flex gap-1">
-                                            {
-                                                    [recipe?.category, recipe?.cuisines, recipe?.mealType]?.map((tag) => (
-                                                        <div className="text-xs px-3 py-1 bg-slate-100 rounded-xl">
-                                                            {tag}
-                                                        </div>
-                                                    ))
-                                            }
-                                        </div>
-
-                                        {/* Recipe rating and cook time */}
-                                        <div className="flex gap-3 items-center text-sm font-normal text-[#666666]">
-                                            <div className="flex items-center gap-1">
-                                                <AccessTime fontSize="small"/>
-                                                {`${recipe?.cookingTime} mins`}
-                                            </div>
-
-                                            <div className="flex items-center gap-1">
-                                                <Groups fontSize="small"/>
-                                                {`${recipe?.serves} servings`}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
                 </div>
             </div>
         </div>
